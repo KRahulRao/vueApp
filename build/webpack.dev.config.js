618717f02d224bdef69010036e304daf64ee8ff5
KRahulRao <krrao93@gmail.com>
2019-12-23 23:47:46 +0530

    adding build folder

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,21 @@
+const webpack  = require('webpack');
+const clientConfig  = require('./webpack.client.config');
+
+module.exports = function setupDevServer(app){
+  const clientCompiler =  webpack(clientConfig);
+
+  clientConfig.entry.app =  [
+    "webpack-dev-middleware/client",
+    clientConfig.entry.app
+  ];
+  clientConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  );
+  app.use(require('webpack-dev-middleware')(clientCompiler,{
+    stats:{
+          colors:true
+    }
+  }));
+  app.use(require('webpack-hot-middleware')(clientCompiler));
+}
